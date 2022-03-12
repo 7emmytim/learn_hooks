39f7fa72c34a1302e62132c073237b2f45429533
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2022-03-12 18:36:06 +0100

    use effect tutorial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import { useEffect, useState } from 'react'
+
 const App = () => {
+
+  const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+  const [windowSize, setWindowSize] = useState(window.innerWidth)
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+
+      return () => console.log('cleaned up ' + resourceType)
+  }, [resourceType])
+
+  const handleResize = () => setWindowSize(window.innerWidth)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      // console.log('cleanup')
+    }
+  }, [])
+
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{windowSize}</h1>
+      <h1>{resourceType}</h1>
+      {
+        items?.map(item => {
+          return <pre key={item.id}>{JSON.stringify(item)}</pre>
+        })
+      }
+    </>
   )
 }
 
