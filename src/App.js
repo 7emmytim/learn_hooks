c150ae6cf91fc82cdfb8e287b7122e432b819041
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2022-03-13 16:55:23 +0100

    use reducer beginning tutorial
    git add -A

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+
 const App = () => {
+
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(prev => prev + 1)
+  const decrement = () => setCount(prev => prev - 1)
+
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </>
   )
 }
 
